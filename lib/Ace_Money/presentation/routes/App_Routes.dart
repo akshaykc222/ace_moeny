362873f3a09b66c87ef445fc695d37316b155ced
jishnukp2482 <jishnuvidyadharan123@gmail.com>
2023-06-26 14:01:18 +0530

    projects

--- lib/Ace_Money/presentation/routes/App_Routes.dart
+++ lib/Ace_Money/presentation/routes/App_Routes.dart
@@ -2,22 +2,18 @@ import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'package:untitled7/Ace_Money/presentation/pages/homepage.dart';
 import 'package:untitled7/Ace_Money/presentation/routes/App_Pages.dart';
 
-class AppRoutes{
-static List<GetPage> routes = [
-  ///Shibin
-  GetPage(name: AppPages.homepage, page: ()=> HomePage()),
+import '../pages/payContacts.dart';
 
+class AppRoutes {
+  static List<GetPage> routes = [
+    ///Shibin
+    GetPage(name: AppPages.homepage, page: () => HomePage()),
 
+    ///Shinas
+    ///
+    ///
 
-
-
-
-
-
-
-
-
-  ///Shinas
-  ///
-];
+    ///jishnu
+    GetPage(name: AppPages.paycontacts, page: () => PayContacts()),
+  ];
 }
