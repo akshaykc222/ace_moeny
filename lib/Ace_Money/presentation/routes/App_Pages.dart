49a3eff2e27a50d7f726f9e684e1b4adbeaa5e6e
Shibin <shibinm129@gmail.com>
2023-06-26 13:26:16 +0530

    folders created

---
+++ lib/Ace_Money/presentation/routes/App_Pages.dart
@@ -0,0 +1,3 @@
+class AppPages{
+  static const homepage = '/homepage';
+}
---
+++ lib/Ace_Money/presentation/routes/App_Routes.dart
@@ -0,0 +1,9 @@
+import 'package:get/get_navigation/src/routes/get_route.dart';
+import 'package:untitled7/Ace_Money/presentation/pages/homepage.dart';
+import 'package:untitled7/Ace_Money/presentation/routes/App_Pages.dart';
+
+class AppRoutes{
+static List<GetPage> routes = [
+  GetPage(name: AppPages.homepage, page: ()=> HomePage()),
+];
+}
