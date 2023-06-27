ad40362c6c2913fef09fc35c4cbd1b25d894a1f8
MUHAMMED-SHINAS <shinasms20@gmail.com>
2023-06-27 13:16:31 +0530

    shinas updated

--- lib/Ace_Money/presentation/pages/homepage.dart
+++ lib/Ace_Money/presentation/pages/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:untitled7/Ace_Money/presentation/themes/app_colors.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,7 +13,7 @@ class HomePage extends StatelessWidget {
         children: [
           Container(
             decoration: BoxDecoration(
-              color: 
+              color: AppColors.primaryColor,
             ),
           )
         ],
