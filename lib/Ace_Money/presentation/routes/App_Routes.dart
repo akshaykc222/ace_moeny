288b6075d9bc84f1990817680b61b16f0fc44346
MUHAMMED-SHINAS <shinasms20@gmail.com>
2023-07-01 11:33:48 +0530

    shinas updated

--- lib/Ace_Money/presentation/routes/App_Routes.dart
+++ lib/Ace_Money/presentation/routes/App_Routes.dart
@@ -2,10 +2,8 @@ import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'package:untitled7/Ace_Money/presentation/pages/addBankAccount.dart';
 import 'package:untitled7/Ace_Money/presentation/pages/qrScanPage.dart';
 import 'package:untitled7/Ace_Money/presentation/routes/App_Pages.dart';
-
 import '../pages/dashboard.dart';
 import '../pages/payContacts.dart';
-
 class AppRoutes {
   static List<GetPage> routes = [
     ///Shibin
@@ -13,7 +11,6 @@ class AppRoutes {
 
     ///Shinas
     GetPage(name: AppPages.qrScanPage, page: ()=> QrScanPage()),
-
     GetPage(name: AppPages.addBankAccount, page: ()=> AddBankAccount()),
 
     ///jishnu
