7dee7f13295be6840c38d565ebe871c4ccd5583b
MUHAMMED-SHINAS <shinasms20@gmail.com>
2023-07-01 11:37:28 +0530

    shinas updated

--- lib/Ace_Money/presentation/routes/App_Routes.dart
+++ lib/Ace_Money/presentation/routes/App_Routes.dart
@@ -1,4 +1,6 @@
 import 'package:get/get_navigation/src/routes/get_route.dart';
+import 'package:untitled7/Ace_Money/presentation/Bindings/AddBankAccountControllerBinding.dart';
+import 'package:untitled7/Ace_Money/presentation/Bindings/QrScanPageControllerBinding.dart';
 import 'package:untitled7/Ace_Money/presentation/pages/addBankAccount.dart';
 import 'package:untitled7/Ace_Money/presentation/pages/qrScanPage.dart';
 import 'package:untitled7/Ace_Money/presentation/routes/App_Pages.dart';
@@ -10,8 +12,8 @@ class AppRoutes {
     GetPage(name: AppPages.dashboard, page: () => DashBoard()),
 
     ///Shinas
-    GetPage(name: AppPages.qrScanPage, page: ()=> QrScanPage()),
-    GetPage(name: AppPages.addBankAccount, page: ()=> AddBankAccount()),
+    GetPage(name: AppPages.qrScanPage, page: ()=> QrScanPage(),bindings: [QrScanControllerBinding()]),
+    GetPage(name: AppPages.addBankAccount, page: ()=> AddBankAccount(),bindings: [AddBankAccountControllerBinding()]),
 
     ///jishnu
     GetPage(name: AppPages.paycontacts, page: () => PayContacts()),
